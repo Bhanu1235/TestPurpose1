7f68e0e79f3f0961e016bb417119d122b76c62b4
Bhanu1235 <34469855+Bhanu1235@users.noreply.github.com>
2019-04-05 12:36:07 +0530

    Test

---
+++ TestPurpose/src/test/java/TestPurpose/NewTest.java
@@ -0,0 +1,17 @@
+package TestPurpose;		
+
+import org.openqa.selenium.WebDriver;		
+import org.openqa.selenium.firefox.FirefoxDriver;		
+import org.testng.Assert;		
+import org.testng.annotations.Test;	
+import org.testng.annotations.BeforeTest;	
+import org.testng.annotations.AfterTest;		
+public class NewTest {		
+	    private WebDriver driver;		
+		@Test				
+		public void testEasy() {	
+							 
+			System.out.println("Test Purpose1");
+			
+		}		
+}
